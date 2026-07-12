3df51af23467726c5a4765cbc98a070365b7d08d
lsholobela-cell <lsholobela@gmail.com>
2026-07-12 21:10:39 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello GitHub!" << endl;
+    cout << "My name is Lushomo." << endl;
+    cout << "This is my first GitHub project!" << endl;
+
+    return 0;
+}
